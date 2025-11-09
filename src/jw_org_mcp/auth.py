988694b0556ea64795c0aa4bb4fc72d713827268
Bjern Potgieter <bjern@toxzen.co.za>
2025-11-09 16:55:34 +0200

    Fixes to auth, finding the CDN

--- src/jw_org_mcp/auth.py
+++ src/jw_org_mcp/auth.py
@@ -36,9 +36,22 @@ class AuthManager:
                 headers={
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                     "AppleWebKit/537.36 (KHTML, like Gecko) "
-                    "Chrome/120.0.0.0 Safari/537.36",
+                    "Chrome/131.0.0.0 Safari/537.36",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
+                    "image/avif,image/webp,image/apng,*/*;q=0.8",
                     "Accept-Language": "en-US,en;q=0.9",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Connection": "keep-alive",
+                    "Upgrade-Insecure-Requests": "1",
+                    "Sec-Fetch-Dest": "document",
+                    "Sec-Fetch-Mode": "navigate",
+                    "Sec-Fetch-Site": "none",
+                    "Sec-Ch-Ua": '"Google Chrome";v="131", "Chromium";v="131", '
+                    '"Not_A Brand";v="24"',
+                    "Sec-Ch-Ua-Mobile": "?0",
+                    "Sec-Ch-Ua-Platform": '"Windows"',
                 },
+                follow_redirects=True,
             )
         return self._http_client
 
